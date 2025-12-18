45c8fbb02896e7442c098c90904bff66f7f65981
Ronald Mathews <ronaldmathews14@gmail.com>
2025-12-19 01:21:15 +0300

    Navbar Project

--- Responsive Navigation Bar/navbar.js
+++ Responsive Navigation Bar/navbar.js
@@ -1,2 +1,17 @@
 
+// Initialize the bars icon
+const toggleBtn = document.querySelector(".toggleBtn");
 
+// Initialise the navigation bar
+const links = document.querySelector(".links");
+
+
+// Clicking the button to toggle the navigation bar
+toggleBtn.addEventListener("click", ()=>{
+
+    if (links.classList.contains("active")){
+        links.classList.remove("active");
+    } else {
+        links.classList.add("active");
+    }
+});
