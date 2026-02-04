942d856d5786b4519b85a31a6e32646e01ce20f8
MASIBO <ronaldmathews14@gmail.com>
2026-02-04 16:07:16 +0300

    A side panel that is toggleable. Has many similarities to 'Side Navigation'

---
+++ Collapsed Sidepanel/colSide.js
@@ -0,0 +1,11 @@
+
+
+
+function openNav(){
+    document.getElementById("mySidePanel").style.width = "250px";
+}
+
+
+function closeNav(){
+    document.getElementById("mySidePanel").style.width = "0";
+}
